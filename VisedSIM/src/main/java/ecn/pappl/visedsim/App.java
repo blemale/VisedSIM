dbc6b6c6ed51d022def095cd5bd6b400627d0115
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-01-19 16:50:20 +0100

    Implémentation dans la classe App d'un main pour tester la (dé)sérialisation xml.

--- VisedSIM/src/main/java/ecn/pappl/visedsim/App.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/App.java
@@ -1,13 +1,33 @@
 package ecn.pappl.visedsim;
 
+import ecn.pappl.visedsim.struct.Project;
+import ecn.pappl.visedsim.struct.ProjectList;
+import ecn.pappl.visedsim.utilities.XMLTools;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws FileNotFoundException, IOException
     {
-        System.out.println( "Hello World!" );
+        Project p1 = new Project();
+        p1.setAcronym("PPP1");
+        p1.setTitle("MonProjet1");
+        
+        Project p2 = new Project();
+        p1.setAcronym("PPP2");
+        p1.setTitle("MonProjet2");
+        
+        ProjectList pl = new ProjectList();
+        ArrayList<Project> l = new ArrayList<Project>();
+        l.add(p1); l.add(p2);
+        pl.setProjectList(l);
+        
+        XMLTools.encodeToFile(l, "MaListeDeProjets.xml");
     }
 }
