ecff495e8b2655a069fa3e735e299a25cc7cb7b3
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-01-19 16:18:12 +0100

    Création de l'objet ProjectList

---
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/struct/ProjectList.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecn.pappl.visedsim.struct;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author bastien
+ */
+public class ProjectList {
+    private ArrayList<Project> projectList;
+
+    public ArrayList<Project> getProjectList() {
+        return projectList;
+    }
+
+    public void setProjectList(ArrayList<Project> projectList) {
+        this.projectList = projectList;
+    }
+    
+    
+}
