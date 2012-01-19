b2e21625207557201f3fdc9e92baea70c2a30e07
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-01-19 14:53:28 +0100

    Commit initial (création du projet maven)

---
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/App.java
@@ -0,0 +1,13 @@
+package ecn.pappl.visedsim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
