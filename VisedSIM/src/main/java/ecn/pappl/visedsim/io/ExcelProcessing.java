dcbe457ca176581e2a0d2f0b16992ee6659ac997
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-01-21 18:35:08 +0100

    Implémentation de la méthode de parsage du fichier Excel dans la Classe ExcelProcessing (la structure de données de sortie est une Map)

--- VisedSIM/src/main/java/ecn/pappl/visedsim/io/ExcelProcessing.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/io/ExcelProcessing.java
@@ -6,6 +6,13 @@ package ecn.pappl.visedsim.io;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 
 /**
@@ -14,30 +21,34 @@ import org.apache.poi.ss.usermodel.*;
  */
 public class ExcelProcessing {
 
-    public static void procFile(File file) {
-        try {
-            Workbook workbook = WorkbookFactory.create(new FileInputStream(file));
-            int numberOfSheets = workbook.getNumberOfSheets();
-            for (int i = 0; i < numberOfSheets; i++) {
-                Sheet sheet = workbook.getSheetAt(i);
-                for (Row row : sheet) {
-                    for (Cell cell : row) {
-                        String message = String.format(
-                                "%s, Sheet[%s], Row[%d], Cell[%d], Contents[%s]",
-                                file.getName(),
-                                sheet.getSheetName(),
-                                row.getRowNum(),
-                                cell.getColumnIndex(),
-                                cellToString(cell));
-                        System.out.println(message);
-                    }
-                }
+    /**
+     * Extracts the datas of an Excel {@link Sheet} and saves them into a {@link Map} 
+     * @param file the {@link File} to parse.
+     * @return the {@link Map} containing the Excel {@link Sheet}'s datas 
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws InvalidFormatException 
+     */
+    public static Map<Integer, Map<Integer, String>> procFile(File file) throws FileNotFoundException, IOException, InvalidFormatException {
+        Map<Integer, Map<Integer, String>> datas = new HashMap<Integer, Map<Integer, String>>();
+        Workbook workbook = WorkbookFactory.create(new FileInputStream(file));
+        Sheet sheet = workbook.getSheetAt(0);
+        for (Row row : sheet) {
+            int rowNum = row.getRowNum();
+            datas.put(rowNum, new HashMap<Integer, String>());
+            for (Cell cell : row) {
+                datas.get(rowNum).put(cell.getColumnIndex(), cellToString(cell));
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
+        return datas;
     }
 
+    /**
+     * Converts the value of a {@link Cell} into a {@link String}.
+     * @param cell the {@link Cell} to convert.
+     * @return the {@link String} representing the {@link Cell} value. 
+     */
     private static String cellToString(Cell cell) {
         String cellToString = "";
         if (cell != null) {
