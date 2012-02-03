508be10bd351c0c140c658f3515c844b9750a83c
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-02-03 14:59:50 +0100

    MapCleaner : JavaDoc

--- VisedSIM/src/main/java/ecn/pappl/visedsim/io/MapCleaner.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/io/MapCleaner.java
@@ -7,17 +7,28 @@ package ecn.pappl.visedsim.io;
 import java.util.Map;
 
 /**
- *
+ * Class processing the cleaning of a {@link Map}.
+ * 
  * @author bastien
  */
 public class MapCleaner {
     
+    /**
+     * Removes a row from a given {@link Map}.
+     * @param map The given {@link Map} to clean.
+     * @param rowIndex The index of the row to remove.
+     */
     public static void removeRow(Map<Integer,Map<Integer,String>> map, int rowIndex){
         if(map.containsKey(rowIndex)){
             map.remove(rowIndex);
         }
     }
     
+    /**
+     * Remove a column from a given {@link Map}.
+     * @param map A given {@link Map} to clean.
+     * @param colIndex The index of the column to remove.
+     */
     public static void removeCol(Map<Integer,Map<Integer,String>> map, int colIndex){
         for(Integer rowIndex : map.keySet()){
             Map<Integer,String> row = map.get(rowIndex);
