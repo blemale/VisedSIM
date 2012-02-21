2fbd4618b267372bba0045e20de795d22f537e4e
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-02-21 11:15:32 +0100

    ProjectListExcelLoader : Création

---
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/controller/ProjectListExcelLoader.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecn.pappl.visedsim.controller;
+
+import ecn.pappl.visedsim.struct.ProjectList;
+import java.util.List;
+import java.util.Map;
+import org.apache.poi.ss.usermodel.Cell;
+
+/**
+ * Interface representing a loader of {@link ProjectList} from a Excel file.
+ *
+ * @author bastien
+ */
+public interface ProjectListExcelLoader {
+
+    /**
+     * Loads a {@link ProjectList} from a given Excel file.
+     *
+     * @param fileName the name of the file to load.
+     * @param columnsOrder the order of the columns in the given Excel file.
+     * @param firstCellRow the row's number of the first {@link Cell} to
+     * consider.
+     * @param firstCellColl the columns's number of the first {@link Cell} to
+     * consider.
+     * @return a {@link ProjectList}.
+     */
+    ProjectList loadExcelProjectList(String fileName, Map<String, List<Integer>> columnsOrder, int firstCellRow, int firstCellColl);
+}
