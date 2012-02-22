1c9c3d9d7494c55556dd7aa91a80d46ecf5dde46
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-02-22 16:05:36 +0100

    CriteriaPreselectionSaver : Création

---
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/controller/criteriapreselection/CriteriaPreselectionSaver.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecn.pappl.visedsim.controller.criteriapreselection;
+
+import ecn.pappl.visedsim.struct.CriteriaPreselection;
+
+/**
+ * Interface representing a saver of {@link CriteriaPreselection}.
+ *
+ * @author bastien
+ */
+public interface CriteriaPreselectionSaver {
+
+    /**
+     * Saves the current {@link CriteriaPreselection} at the given path.
+     *
+     * @param fileName the path where to save the current {@link CriteriaPreselection}.
+     */
+    void saveCriteriaPreselection(String fileName);
+}
