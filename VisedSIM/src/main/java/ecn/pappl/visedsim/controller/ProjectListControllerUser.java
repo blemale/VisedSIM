6ca31827f5fa3ccf1ecd37fe4e2d5b7a77f60181
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-02-22 14:44:36 +0100

    ProjectListControllerUser : Création

---
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/controller/ProjectListControllerUser.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecn.pappl.visedsim.controller;
+
+/**
+ * Class representing a controller for {@link ProjectList} in User mode.
+ * 
+ * @author bastien
+ */
+public class ProjectListControllerUser extends ProjectListController {
+
+    /**
+     * Default constructor.
+     */
+    public ProjectListControllerUser() {
+        super();
+    }
+}
