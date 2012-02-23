33b7ffe41b0b03c45d30c9bb15028aa6a2f27f34
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-02-23 10:50:06 +0100

    ProjectListSaver : Nouvelle méthode saveProjectLListWithInterrestConflicts

--- VisedSIM/src/main/java/ecn/pappl/visedsim/controller/projectlist/ProjectListSaver.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/controller/projectlist/ProjectListSaver.java
@@ -4,8 +4,10 @@
  */
 package ecn.pappl.visedsim.controller.projectlist;
 
+import ecn.pappl.visedsim.struct.CriteriaPreselection;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Interface repesenting a saver for {@link ProjectList}.
@@ -20,5 +22,25 @@ public interface ProjectListSaver {
      * @param fileName the path where to save the current {@link CriteriaPreselection}.
      */
     void saveProjectList(String fileName) throws FileNotFoundException,
-                                                 IOException;
+            IOException;
+
+    /**
+     * Saves the current {@link ProjectList} at the given path and considering
+     * conflicts of interrest.
+     * <p/>
+     * @param fileName the path where to save the current {@link CriteriaPreselection}.
+     * @param conflicts a {@link Map} which represents the conflicts of
+     * interrest. The keys are the acronyms of projects, and the value is true
+     * if there is a conflict and false else.
+     * @param conflictCriteria the {@link CriteriaPreselection} to apply for
+     * project with conflict.
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    void saveProjectListWithInterrestConflicts(String fileName,
+            Map<String, Boolean> conflicts,
+            CriteriaPreselection conflictCriteria) throws
+            FileNotFoundException,
+            IOException;
+;
 }
