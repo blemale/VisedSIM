d776e2e2b37f3ec3fff3af2d3c7d2b1b1fec4f3c
denisdelsol <Denis@.(none)>
2012-02-25 12:37:16 +0100

    Classe contenant l'ensemble des libellés

---
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/view/Labels.java
@@ -0,0 +1,16 @@
+package ecn.pappl.visedsim.view;
+
+/**
+ * Contains the different strings used in the GUI
+ * 
+ * @author Denis
+ */
+public final class Labels {
+    
+    //FileLoader
+    public final static String LIST_PROJECT = "Liste des projets";
+    public final static String NEW_PROJECT_LIST = "Nouvelle liste de projets";
+    public final static String CHOOSE_NEW_LIST_BUTTON = "Choisir...";
+    public final static String LOAD_SAVED_LIST_BUTTON = "Charger";
+    public final static String LOAD_NEW_LIST_BUTTON = "Charger";
+}
