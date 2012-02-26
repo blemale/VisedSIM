27dcc367eccd2883c0bec081ae3e0b1eaa6a7b7a
denisdelsol <Denis@.(none)>
2012-02-26 22:40:36 +0100

    Ajouts de libellés pour le choix des critères et le choix des projets en conflits

--- VisedSIM/src/main/java/ecn/pappl/visedsim/view/Labels.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/view/Labels.java
@@ -8,9 +8,24 @@ package ecn.pappl.visedsim.view;
 public final class Labels {
     
     //FileLoader
+    public final static String FILE_LOADER_TITLE = "Chargement d'une liste de projets";
     public final static String LIST_PROJECT = "Liste des projets";
     public final static String NEW_PROJECT_LIST = "Nouvelle liste de projets";
     public final static String CHOOSE_NEW_LIST_BUTTON = "Choisir...";
     public final static String LOAD_SAVED_LIST_BUTTON = "Charger";
     public final static String LOAD_NEW_LIST_BUTTON = "Charger";
+    
+    //ChooseCriteria
+    public final static String CHOOSE_CRITERIA_TITLE = "Choix des critères";
+    public final static String SELECT_ALL_BUTTON = "Sélectionner tout";
+    public final static String RESET_BUTTON = "Reset";
+    public final static String PRESELECTION_VALIDATION_BUTTON = "Vadider";
+    public final static String VALIDATION_BUTTON = "Valider";
+    public final static String CANCEL_BUTTON = "Annuler";
+    
+    //ConfidentialProjects
+    public final static String CONFIDENTIAL_PROJECTS_TITLE = "Imprimer - Projets en conflits";
+    public final static String CONFIDENTIAL_PROJECTS_LABEL = "Quels sont les projets en conflits ?";
+    public final static String CONFIDENTIAL_PROJECTS_VALIDATION = "Valider";
+    public final static String CONFIDENTIAL_PROJECTS_CANCEL = "Annuler";
 }
