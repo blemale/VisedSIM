28276c843b98684c03a85dbccd4b9b40adef245a
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-03-03 16:26:26 +0100

    ProjectListController: Refactoring (supressio ProjectListControllerAdmin et User)

--- VisedSIM/src/main/java/ecn/pappl/visedsim/controller/projectlist/ProjectListController.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/controller/projectlist/ProjectListController.java
@@ -4,21 +4,31 @@
  */
 package ecn.pappl.visedsim.controller.projectlist;
 
+import ecn.pappl.visedsim.io.ExcelDatasExtractor;
+import ecn.pappl.visedsim.io.MapCleaner;
+import ecn.pappl.visedsim.io.MapConverter;
+import ecn.pappl.visedsim.struct.CriteriaPreselection;
 import ecn.pappl.visedsim.struct.Project;
 import ecn.pappl.visedsim.struct.ProjectList;
+import ecn.pappl.visedsim.utilities.ProjectTools;
 import ecn.pappl.visedsim.utilities.XMLTools;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 /**
  * Abstract class representing a controller for a {@link ProjectList}.
- * 
+ * <p/>
  * @author bastien
  */
-public abstract class ProjectListController implements ProjectListLoader, ProjectListSelector {
+public class ProjectListController implements ProjectListLoader,
+        ProjectListSelector, ProjectListExcelLoader, ProjectListSaver {
 
     /**
      * The {@link ProjectList} to control.
@@ -32,7 +42,8 @@ public abstract class ProjectListController implements ProjectListLoader, Projec
         super();
     }
 
-    public ProjectList loadProjectList(String fileName) throws FileNotFoundException, IOException {
+    public ProjectList loadProjectList(String fileName) throws
+            FileNotFoundException, IOException {
         this.projectList = (ProjectList) XMLTools.decodeFromFile(fileName);
         return this.projectList;
     }
@@ -69,12 +80,61 @@ public abstract class ProjectListController implements ProjectListLoader, Projec
         } else {
             List<String> list = new LinkedList<String>();
             for (Project project : this.projectList.getProjectList()) {
-                if(project.getAcronym().startsWith(firstLetters)){
+                if (project.getAcronym().startsWith(firstLetters)) {
                     list.add(project.getAcronym());
                 }
             }
             Collections.sort(list);
             return list;
         }
+    }
+
+    public ProjectList loadExcelProjectList(String fileName,
+            Map<String, List<Integer>> columnsOrder,
+            int firstCellRow, int firstCellColl)
+            throws FileNotFoundException, IOException, InvalidFormatException,
+            NoSuchMethodException, IllegalAccessException,
+            IllegalArgumentException, InvocationTargetException {
+        Map<Integer, Map<Integer, String>> map =
+                ExcelDatasExtractor.procFile(new File(fileName));
+        for (int i = 0; i < firstCellRow; i++) {
+            MapCleaner.removeRow(map, i);
+        }
+        for (int j = 0; j < firstCellColl; j++) {
+            MapCleaner.removeCol(map, j);
+        }
+        this.projectList = MapConverter.convertMapToProjectList(map,
+                columnsOrder);
+        return this.projectList;
+    }
+
+    public void saveProjectList(String fileName) throws FileNotFoundException,
+            IOException {
+        if (this.projectList != null) {
+            XMLTools.encodeToFile(this.projectList, fileName);
+        }
+    }
+
+    public void saveProjectListWithInterrestConflicts(String fileName,
+            Map<String, Boolean> conflicts,
+            CriteriaPreselection conflictCriteria) throws FileNotFoundException,
+            IOException,
+            IllegalArgumentException,
+            IllegalAccessException {
+        if (this.projectList != null) {
+            ProjectList newProjectList = new ProjectList();
+            List<Project> newProjects = newProjectList.getProjectList();
+            for (Project project : this.projectList.getProjectList()) {
+                if (conflicts.get(project.getAcronym()) == true) {
+                    Project buffer = ProjectTools.applyCriteriaPreselection(
+                            project, conflictCriteria);
+                    newProjects.add(buffer);
+                } else {
+                    newProjects.add(project);
+                }
+            }
+            XMLTools.encodeToFile(newProjectList, fileName);
+        }
+
     }
 }
