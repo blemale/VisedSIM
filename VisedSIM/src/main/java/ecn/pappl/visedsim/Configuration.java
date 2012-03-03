1731841bdaf7be057e97662d5ed62a5a037d8be3
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-03-03 16:39:29 +0100

    Configuration : création

---
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/Configuration.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecn.pappl.visedsim;
+
+/**
+ *
+ * @author bastien
+ */
+public final class Configuration {
+    public final static String CRITERIA_PRESELECTION_FOLDER = "";
+    public final static String PROJECT_LIST_FOLDER = "";
+}
