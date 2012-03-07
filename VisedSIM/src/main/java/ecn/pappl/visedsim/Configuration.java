e25c60564a5c9c407f87113dbc8badedd36ccb94
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-03-07 22:32:19 +0100

    Configuration : Refactoring

--- VisedSIM/src/main/java/ecn/pappl/visedsim/Configuration.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/Configuration.java
@@ -12,5 +12,5 @@ public final class Configuration {
     public final static String CRITERIA_PRESELECTION_FOLDER = "";
     public final static String PROJECT_LIST_FOLDER = "";
     public final static String COLUMNS_ORDER_FILE_PATH = "";
-    public final static String DEFAULT_CRITERIA_PRESELECTION_PATH = "";
+    public final static String DEFAULT_CRITERIA_PRESELECTION_PATH = "default_criteria_preselection";
 }
