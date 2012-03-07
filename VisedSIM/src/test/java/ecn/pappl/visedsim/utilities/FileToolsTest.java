fe2c987185c4ef01a912f6b736bbed802c500511
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-03-07 21:35:56 +0100

    FileToolsTest : Refactoring

--- VisedSIM/src/test/java/ecn/pappl/visedsim/utilities/FileToolsTest.java
+++ VisedSIM/src/test/java/ecn/pappl/visedsim/utilities/FileToolsTest.java
@@ -34,8 +34,7 @@ public class FileToolsTest extends TestCase {
     public void testGetFilesNamesInDirectory() {
         System.out.println("getFilesNamesInDirectory");
         String directoryPath = getClass().getClassLoader().getResource(
-                "testFolder/file1").getPath();
-        directoryPath = directoryPath.split("file1")[0];       
+                "testFolder").getPath();       
         List<String> expResult = new LinkedList<String>();
         expResult.add("file1"); expResult.add("file2");
         List<String> result = FileTools.getFilesNamesInDirectory(directoryPath);
