106c201b2d2ef9705bc808f54f7b6b19549c7795
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-03-07 15:24:17 +0100

    ProjectTools : Ajout méthode.

--- VisedSIM/src/main/java/ecn/pappl/visedsim/utilities/ProjectTools.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/utilities/ProjectTools.java
@@ -7,6 +7,7 @@ package ecn.pappl.visedsim.utilities;
 import ecn.pappl.visedsim.struct.CriteriaPreselection;
 import ecn.pappl.visedsim.struct.Project;
 import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -21,16 +22,63 @@ public final class ProjectTools {
             CriteriaPreselection criteriaPreselection) throws
             IllegalArgumentException, IllegalAccessException {
         Project newProject = new Project();
-        Field[] fields = project.getClass().getFields();
+        Field[] fields = project.getClass().getDeclaredFields();
         Map<String, Boolean> map = criteriaPreselection.getMap();
         for (int i = 0; i < fields.length; i++) {
             Field field = fields[i];
             if (((Boolean) map.get(field.getName())) == true) {
+                field.setAccessible(true);
                 Object o = field.get(project);
                 field.set(newProject, o);
             }
         }
         return newProject;
     }
-;
+
+    public static int getNumberOfCriteriaLines(Project project,
+            CriteriaPreselection cp) throws IllegalArgumentException,
+            IllegalAccessException {
+        int count = 0;
+        Map<String, Boolean> map = cp.getMap();
+        Field[] fields = project.getClass().getDeclaredFields();
+        for (int i = 0; i < fields.length; i++) {
+            Field field = fields[i];
+            if (((Boolean) map.get(field.getName())) == true) {
+                if (field.getType().equals(String.class)) {
+                    count++;
+                } else if (field.getType().equals(List.class)) {
+                    field.setAccessible(true);
+                    List l = (List) field.get(project);
+                    count += l.size();
+                }
+            }
+        }
+        return count;
+    }
+    
+    public static void fillArrayWithSelectedCriteria(Project project,
+            CriteriaPreselection cp, Object[][] array) throws IllegalArgumentException, IllegalAccessException{
+        int indexLine = 0;
+        Map<String, Boolean> map = cp.getMap();
+        Field[] fields = project.getClass().getDeclaredFields();
+        for (int i = 0; i < fields.length; i++) {
+            Field field = fields[i];
+            if (((Boolean) map.get(field.getName())) == true) {
+                if (field.getType().equals(String.class)) {
+                    array[indexLine][1] = field.getName();
+                    field.setAccessible(true);
+                    array[indexLine][2] = (String)field.get(project);
+                    indexLine++;
+                } else if (field.getType().equals(List.class)) {
+                    field.setAccessible(true);
+                    List l = (List) field.get(project);
+                    for(Object o : l){
+                        array[indexLine][1] = field.getName();
+                        array[indexLine][2] = (String) o;
+                    }
+                    
+                }
+            }
+        }
+    }
 }
