6f115aa02a6d27a274ecbb9e3edbb09790a272a6
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-03-08 18:04:58 +0100

    Configuration : update

--- VisedSIM/src/main/java/ecn/pappl/visedsim/Configuration.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/Configuration.java
@@ -9,9 +9,9 @@ package ecn.pappl.visedsim;
  * @author bastien
  */
 public final class Configuration {
-    public final static String CRITERIA_PRESELECTION_FOLDER = "";
+    public final static String CRITERIA_PRESELECTION_FOLDER = "criteria_preselections";
     public final static String PROJECT_LIST_FOLDER = "";
-    public final static String COLUMNS_ORDER_FILE_PATH = "";
-    public final static String DEFAULT_CRITERIA_PRESELECTION_PATH = "default_criteria_preselection";
-    public final static String INIT_CRITERIA_PRESELECTION_PATH = "init_criteria_preselection";
+    public final static String COLUMNS_ORDER_FILE_PATH = "default_instances/default_columns_order";
+    public final static String DEFAULT_CRITERIA_PRESELECTION_PATH = "default_instances/default_criteria_preselection";
+    public final static String INIT_CRITERIA_PRESELECTION_PATH = "default_instances/init_criteria_preselection";
 }
