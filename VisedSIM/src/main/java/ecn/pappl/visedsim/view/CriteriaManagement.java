bd1e57fe8ecc375223d4af9edf62466f44053172
denisdelsol <Denis@.(none)>
2012-03-08 18:30:35 +0100

    Création de l'interface de gestion des présélections de critères

---
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/view/CriteriaManagement.java
@@ -0,0 +1,110 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecn.pappl.visedsim.view;
+
+import ecn.pappl.visedsim.controller.criteriapreselection.CriteriaPreselectionController;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.*;
+
+/**
+ *
+ * @author Denis
+ */
+public class CriteriaManagement extends JDialog {
+    
+    private Map<String, JCheckBox> preselectionMap;
+    private JButton backButton, deleteButton;
+    private JLabel titleLabel;
+    private List<String> preselectionList;
+    
+    public CriteriaManagement(List<String> preselectionList){
+        super();
+        this.preselectionList = preselectionList;
+        build();
+    }
+    
+    private void build(){
+        setTitle(Labels.CRITERIA_MANAGEMENT_TITLE);
+	setLocationRelativeTo(null);
+	setResizable(false);
+        setModal(true);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setContentPane(buildContentPane());
+	pack();
+    }
+    
+    private JPanel buildContentPane(){
+        JPanel panelCenter = new JPanel();
+        panelCenter.setLayout(new BorderLayout());
+	panelCenter.setBackground(Color.white);
+        
+        JPanel panel = new JPanel(new SpringLayout());
+        panel.setBackground(Color.white);
+        
+        titleLabel = new JLabel(Labels.CRITERIA_MANAGEMENT_LABEL);
+        panel.add(titleLabel);
+        
+        JPanel middlePanel = new JPanel(new SpringLayout());
+        middlePanel.setBackground(Color.white);
+        
+        preselectionMap = new HashMap<String, JCheckBox>();
+        
+        for(String preselection : preselectionList){
+            preselectionMap.put(preselection, new JCheckBox(preselection));
+            preselectionMap.get(preselection).setBackground(Color.white);
+            middlePanel.add(preselectionMap.get(preselection));
+        }
+        
+        SpringUtilities.makeCompactGrid(middlePanel, preselectionList.size(),1, 5, 5, 5, 5);
+        
+        panel.add(panelCenter, BorderLayout.CENTER);
+        
+        JPanel buttonPanel = new JPanel(new SpringLayout());
+        buttonPanel.setBackground(Color.white);
+        
+        backButton = new JButton(Labels.CRITERIA_MANAGEMENT_BACK);
+        backButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backActionEvent(evt);
+            }
+        });
+        buttonPanel.setBackground(Color.white);
+        
+        deleteButton = new JButton(Labels.CRITERIA_MANAGEMENT_DELETE);
+        deleteButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deleteActionEvent(evt);
+            }
+        });
+        buttonPanel.setBackground(Color.white);
+        
+        SpringUtilities.makeCompactGrid(buttonPanel, 1, 2, 5, 5, 10, 10);
+        
+        panel.add(buttonPanel);
+        
+        return panelCenter;
+    }
+    
+    private void deleteActionEvent(java.awt.event.ActionEvent evt){
+        int rs;
+        rs = JOptionPane.showConfirmDialog(this, Labels.CRITERIA_MANAGEMENT_DIALOG, null, JOptionPane.YES_NO_OPTION);
+        if(rs == JOptionPane.YES_OPTION){
+            CriteriaPreselectionController cpc = CriteriaPreselectionController.getInstance();
+            for (String preselection : preselectionList){
+                if (preselectionMap.get(preselection).isSelected()){
+                    cpc.deleteCriteriaPreselection(preselection);
+                }
+            }
+        }
+    }
+    
+    private void backActionEvent(java.awt.event.ActionEvent evt){
+        this.dispose();
+    }
+}
