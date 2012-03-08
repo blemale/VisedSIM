906069265dbaa9a74ee54e40095c1643be04f101
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-03-08 18:11:57 +0100

    App : merge commit

--- VisedSIM/src/main/java/ecn/pappl/visedsim/App.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/App.java
@@ -31,13 +31,9 @@ public class App
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         
         
