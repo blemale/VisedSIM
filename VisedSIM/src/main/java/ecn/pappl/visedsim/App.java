c08e972e6404699afa2b783cf4ede722d367f635
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-03-08 18:07:30 +0100

    App

--- VisedSIM/src/main/java/ecn/pappl/visedsim/App.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/App.java
@@ -1,11 +1,20 @@
 package ecn.pappl.visedsim;
 
+import ecn.pappl.visedsim.struct.CriteriaPreselection;
+import ecn.pappl.visedsim.struct.Project;
+import ecn.pappl.visedsim.utilities.XMLTools;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
 public class App {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {        
     }
 }
