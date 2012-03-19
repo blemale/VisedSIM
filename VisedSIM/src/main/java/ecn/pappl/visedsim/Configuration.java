c5f663970a950aa7e856dc9c9cd3d88cc3801ec7
blemale <bastien.lemale@eleves.ec-nantes.fr>
2012-03-19 11:01:16 +0100

    Configuration : update

--- VisedSIM/src/main/java/ecn/pappl/visedsim/Configuration.java
+++ VisedSIM/src/main/java/ecn/pappl/visedsim/Configuration.java
@@ -9,15 +9,20 @@ package ecn.pappl.visedsim;
  * @author bastien
  */
 public final class Configuration {
-    
+
     public static final Boolean IS_ADMIN = false;
-    public static final String CRITERIA_PRESELECTION_FOLDER = "criteria_preselections";
+    public static final String CRITERIA_PRESELECTION_FOLDER =
+            "criteria_preselections";
     public static final String PROJECT_LIST_FOLDER = "";
-    public static final String COLUMNS_ORDER_FILE_PATH = "default_instances/default_columns_order";
-    public static final String DEFAULT_CRITERIA_PRESELECTION_PATH = "default_instances/default_criteria_preselection";
-    public static final String INIT_CRITERIA_PRESELECTION_PATH = "default_instances/init_criteria_preselection";
+    public static final String DEFAULT_INSTANCES_FOLDER = "default_instances";
+    public static final String COLUMNS_ORDER_FILE_PATH =
+            DEFAULT_INSTANCES_FOLDER + "/default_columns_order";
+    public static final String DEFAULT_CRITERIA_PRESELECTION_PATH = DEFAULT_INSTANCES_FOLDER
+            + "/default_criteria_preselection";
+    public static final String INIT_CRITERIA_PRESELECTION_PATH = DEFAULT_INSTANCES_FOLDER
+            + "/init_criteria_preselection";
     public static final String I18N_FOLDER = "i18n";
-    
-    private Configuration(){
+
+    private Configuration() {
     }
 }
